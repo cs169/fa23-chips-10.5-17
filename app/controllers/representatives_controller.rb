5d004f8560614eea35a0ecbb8d736ed5cfa83c83
Vincent Do <vincentdo12@berkeley.edu>
2023-11-28 07:27:17 +0000

    fixed if address is nil

--- app/controllers/representatives_controller.rb
+++ app/controllers/representatives_controller.rb
@@ -1,9 +1,35 @@
-# frozen_string_literal: true
-
 class RepresentativesController < ApplicationController
   def index
     @representatives = Representative.all
-    # @representatives = Representative.find(params[:id])
-    # @additional_info = fetch_additional_info(@representative.official_id)
+  end
+
+  def show
+    @representative = Representative.find(params[:id])
+
+    # Check if @representative.address is not nil before accessing its attributes
+    if @representative.address
+      @additional_info = {
+        address: {
+          street: @representative.address[:street],
+          city: @representative.address[:city],
+          state: @representative.address[:state],
+          zip: @representative.address[:zip]
+        },
+        party: @representative.party,
+        photo: @representative.photo
+      }
+    else
+      # Handle the case where address is nil (customize as needed)
+      @additional_info = {
+        address: {
+          street: 'N/A',
+          city: 'N/A',
+          state: 'N/A',
+          zip: 'N/A'
+        },
+        party: @representative.party,
+        photo: @representative.photo
+      }
+    end
   end
 end
