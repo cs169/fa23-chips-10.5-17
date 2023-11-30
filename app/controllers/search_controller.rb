70a36895866d9cfdeb8321ee8a7d234c9d24e71a
Vincent Do <vincentdo12@berkeley.edu>
2023-11-30 01:51:37 +0000

    added flash

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,14 +1,20 @@
-# frozen_string_literal: true
-
-require 'google/apis/civicinfo_v2'
-
 class SearchController < ApplicationController
   def search
     address = params[:address]
     service = Google::Apis::CivicinfoV2::CivicInfoService.new
     service.key = Rails.application.credentials[:GOOGLE_API_KEY]
-    result = service.representative_info_by_address(address: address)
-    @representatives = Representative.civic_api_to_representative_params(result)
+
+    begin
+      result = service.representative_info_by_address(address: address)
+      @representatives = Representative.civic_api_to_representative_params(result) || []
+      
+      if @representatives.empty?
+        flash.now[:alert] = "No information found for the given address."
+      end
+    rescue Google::Apis::Error => e
+      puts "Google API Error: #{e.message}"
+      flash.now[:alert] = "Error fetching information."
+    end
 
     render 'representatives/search'
   end
